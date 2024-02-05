369af85d54803c4a551d3f83d577ddc3123d9bd3
Arauz2912 <jonatan.arauz.jarquin@est.una.ac.cr>
2024-02-04 21:36:38 -0600

    correccion

--- Lab#6/Caballero.cpp
+++ Lab#6/Caballero.cpp
@@ -1,3 +1,45 @@
 #include "Caballero.h"'
 
-Caballero::Caballero():Entidad(2), HP(100) {}
+Caballero::Caballero() :Entidad(2), HP(100) {}
+
+Caballero::~Caballero()
+{
+}
+
+int Caballero::getHP()
+{
+	return HP;
+}
+
+void Caballero::setHP(int _HP)
+{
+	HP = _HP;
+}
+
+void Caballero::usarArma()
+{
+	if (inventarioArma != nullptr) {
+
+		int danio = inventarioArma->getTipoDano();
+
+		std::cout << "El caballero ataca, danio " << danio << std::endl;
+
+		//se elimina el arma usada
+
+		delete inventarioArma;
+		inventarioArma = nullptr;
+	}
+	else {
+		std::cout << "No hay arma" << std::endl;
+	}
+}
+
+void Caballero::agregarArma(Arma* nuevaArma)
+{
+	//para quitar si hay un arma equipada
+	if (inventarioArma != nullptr) {
+		delete inventarioArma;
+	}
+
+	inventarioArma = nuevaArma;
+}
