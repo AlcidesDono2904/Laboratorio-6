71f343e9b474df0dc775a5c5e19302db409ab84f
Dwayne1205 <dhuaine.ramirez.leon@est.una.ac.cr>
2024-02-04 18:40:13 -0600

    Correcion-Juegocpp

--- Lab#6/Juego.cpp
+++ Lab#6/Juego.cpp
@@ -1,5 +1,5 @@
 #include "Juego.h"
-#include "Enemigo.h"
+
 
 Juego::Juego(int t1, int t2) {
 
@@ -18,69 +18,92 @@ Juego::Juego(int t1, int t2) {
 
 	}
 }
-	void Juego::iniciar() {
-		
-		for (int i = 0; i < tam1; i++) {
-			for (int j = 0; j < tam2; j++) {
-				if (i == 0) {
-					if (j == 0) {
-						mapa[0][0] = new Entidad(2);
-					}
-					else {
-						mapa[i][j] = new Entidad(0);
-					}
+//void Juego::iniciar() {
+
+	/*for (int i = 0; i < tam1; i++) {
+		for (int j = 0; j < tam2; j++) {
+			if (i == 0) {
+				if (j == 0) {
+					mapa[i][j] = new Caballero;
+					mapa[i][j]->setX(i);
+					mapa[i][j]->setY(j);
+				}
+				else {
+					mapa[i][j] = new Enemigo(0, 1);
+					mapa[i][j]->setX(i);
+					mapa[i][j]->setY(j);
+				}
+			}
+			else
+				if (i < 4) {
+					mapa[i][j] = new Entidad(0);
+					mapa[i][j]->setX(i);
+					mapa[i][j]->setY(j);
 				}
 				else
-					if (i < 4) {
-						mapa[i][j] = new Entidad(0);
-					}
-					else 
-						if(i>=4 && i<7){
-							if (i == 4 && j == 0) {
-								mapa[i][j] = new Entidad(0);
-							}else
+					if (i >= 4 && i < 7) {
+						if (i == 4 && j == 0) {
+							mapa[i][j] = new Entidad(0);
+							mapa[i][j]->setX(i);
+							mapa[i][j]->setY(j);
+						}
+						else
 							mapa[i][j] = new Entidad(1);
-						}else
-							if(i==7){
-								if (j < 1) {
-									mapa[i][j] = new Entidad(1);
-								}
-								if (j >= 1 && j < 7) {
-									mapa[i][j] = new Entidad(3);
-								}if (j == 7) {
-									mapa[i][j] = new Entidad(4);
-									}
+						mapa[i][j]->setX(i);
+						mapa[i][j]->setY(j);
+					}
+					else
+						if (i == 7) {
+							if (j < 1) {
+								mapa[i][j] = new Entidad(1);
+								mapa[i][j]->setX(i);
+								mapa[i][j]->setY(j);
 							}
-							else mapa[i][j] = nullptr;			
+							if (j >= 1 && j < 7) {
+								mapa[i][j] = new Entidad(3);
+								mapa[i][j]->setX(i);
+								mapa[i][j]->setY(j);
+							}if (j == 7) {
+								mapa[i][j] = new Entidad(4);
+								mapa[i][j]->setX(i);
+								mapa[i][j]->setY(j);
+							}
+						}
+						else mapa[i][j] = nullptr;
 
-			}
 		}
-		
 	}
 
-	void Juego::inicializar() {
+}*/
 
-		Entidad* aux;
+void Juego::inicializar() {
 
-		for (int i = 0; i < 9; i++) {
+	Entidad* aux;
+
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 9; j++) {
 			std::random_device rd;
 			std::mt19937 gen(rd());
 			std::uniform_int_distribution<int> distribution1(0, 9);
 			int random = distribution1(gen);
 
-			aux = mapa[i][random];
+			aux = mapa[j][random];
 
-			mapa[i][random] = mapa[random][i];
+			mapa[j][random] = mapa[random][j];
 
-			mapa[random][i] = aux;
+			mapa[random][j] = aux;
 
 		}
 	}
-	Juego::~Juego() {
-		
-		for (int i = 0; i < tam1; i++) {
-			delete[] mapa[i];
-		}
-		delete[] mapa;
-		mapa = nullptr;
+}
+Juego::~Juego() {
+
+	for (int i = 0; i < 9; i++) {
+
+		delete[] mapa[i];
+
 	}
+	delete[] mapa;
+	mapa = nullptr;
+
+}
